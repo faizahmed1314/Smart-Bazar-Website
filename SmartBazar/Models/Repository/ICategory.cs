19e2fbf75c8290d8cc9f43bbba20b90b7f715008
faizahmed1314 <foyezahmedfj20@gmail.com>
2020-04-19 13:16:32 +0600

    Initial commit

---
+++ SmartBazar/Models/Repository/ICategory.cs
@@ -0,0 +1,17 @@
+﻿using SmartBazar.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBazar.Models.Repository
+{
+    interface ICategory
+    {
+        void InsertCategory(Category category);
+        void EditCategory(Category category);
+        void ViewCategory();
+        void GetCategoryById(int id);
+    }
+}
